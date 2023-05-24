8ee4191560c22f95c7c2c8456c15807673943e71
Carlos Daniel <ms_danniel@outlook.com>
2023-05-24 20:52:37 -0300

    Criado interface para pessoa no service core

---
+++ Codigo/Core/Service/IPessoaService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    internal interface IPessoaService
+    {
+        int Create(Pessoa cidadao);
+        void Edit(Pessoa cidadao);
+        void Delete(int id);
+        Pessoa Get(int id);
+        IEnumerable<Pessoa> GetAll();
+    }
+}
