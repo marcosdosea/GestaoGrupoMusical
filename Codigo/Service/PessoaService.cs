2a8a8dfa0006e0236e357cd57a2a1904e6f47789
Carlos Daniel <ms_danniel@outlook.com>
2023-05-24 21:12:28 -0300

    Criada classe service para pessoa

---
+++ Codigo/Service/PessoaService.cs
@@ -0,0 +1,76 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class PessoaService : IPessoaService
+    {
+        private readonly GrupoMusicalContext _context;
+
+        public PessoaService(GrupoMusicalContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Metodo que cria uma nova entidade pessoa/associado
+        /// </summary>
+        /// <param name="pessoa">dados do novo associado</param>
+        /// <returns>retorna o id referente a nova entidade criada</returns>
+        public int Create(Pessoa pessoa)
+        {
+            _context.Add(pessoa);
+            _context.SaveChanges();
+            return pessoa.Id;
+        }
+
+        /// <summary>
+        /// Metodo que atualiza os dados de uma pessoa/associado
+        /// </summary>
+        /// <param name="pessoa">dados do associado</param>
+        public void Edit(Pessoa pessoa)
+        {
+            //Criar excecao para data de nascimento, etc
+            _context.Update(pessoa);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Metodo que remove/deleta uma pessoa/associado
+        /// </summary>
+        /// <param name="id">id do alvo a ser deletado/removido</param>
+        public void Delete(int id)
+        {
+            var pessoa = _context.Pessoas.Find(id);
+            _context.Remove(pessoa);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Metodo que obtem informacoes de uma pessoa/associado pelo
+        /// id
+        /// </summary>
+        /// <param name="id">id do alvo</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Pessoa Get(int id)
+        {
+            return _context.Pessoas.Find(id);
+        }
+
+        /// <summary>
+        /// Metodo que retorna todos as pessoas cadastradas no sistema
+        /// </summary>
+        /// <returns>lista com todas as pessoas</returns>
+        public IEnumerable<Pessoa> GetAll()
+        {
+            return _context.Pessoas.AsNoTracking();
+        }
+    }
+}
