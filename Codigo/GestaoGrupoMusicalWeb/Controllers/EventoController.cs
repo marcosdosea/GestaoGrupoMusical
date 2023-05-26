46c852bf3cf20b9fcb5050b8013ad8fdbbf1cbbf
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2023-05-26 10:48:02 -0300

    Arquivos da implementação do Caso de Uso Manter Eventos Apresentação. #12

---
+++ Codigo/GestaoGrupoMusicalWeb/Controllers/EventoController.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using GestaoGrupoMusicalWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoGrupoMusicalWeb.Controllers
+{
+
+    public class EventoController : Controller
+    {
+        private readonly IEvento _evento;
+        private readonly IMapper _mapper;
+
+        public EventoController(IEvento grupoMusical, IMapper mapper)
+        {
+            _evento = grupoMusical;
+            _mapper = mapper;
+        }
+
+        // GET: EventoController
+        public ActionResult Index()
+        {
+            var listaEvento = _evento.GetAll();
+            var listaEventoModel = _mapper.Map<List<EventoViewModel>>(listaEvento);
+            return View(listaEventoModel);
+        }
+
+        // GET: EventoController/Details/5
+        public ActionResult Details(int id)
+        {
+            var evento = _evento.Get(id);
+            var eventoModel = _mapper.Map<EventoViewModel>(evento);
+            return View(eventoModel);
+        }
+
+        // GET: EventoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EventoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EventoViewModel eventoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var eventoModel = _mapper.Map<Evento>(eventoViewModel);
+                _evento.Create(eventoModel);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: EventoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var evento = _evento.Get(id);
+            var eventoModel = _mapper.Map<GrupoMusicalViewModel>(evento);
+
+            return View(eventoModel);
+        }
+
+        // POST: EventoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, EventoViewModel eventoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var evento = _mapper.Map<Evento>(eventoViewModel);
+                _evento.Edit(evento);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: EventoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var evento = _evento.Get(id);
+            var eventoModel = _mapper.Map<EventoViewModel>(evento);
+            return View(eventoModel);
+        }
+
+        // POST: EventoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, EventoViewModel eventoViewModel)
+        {
+            _evento.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Codigo/Service/EventoService.cs
@@ -0,0 +1,59 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service
+{
+    public class EventoService : IEvento
+    {
+        private readonly GrupoMusicalContext _context;
+
+        public EventoService(GrupoMusicalContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Método usado a para adicionar uma nova apresentação
+        /// </summary>
+        /// <param name="evento"></param>
+        /// <returns>Id do Grupo Musical</returns>
+        public int Create(Evento evento)
+        {
+            _context.Add(evento);
+            _context.SaveChanges();
+            return evento.Id;
+        }
+        /// <summary>
+        /// Método que deleta uma apresentação 
+        /// </summary>
+        /// <param name="id"></param>
+        public void Delete(int id)
+        {
+            var evento = _context.Eventos.Find(id);
+            _context.Remove(evento);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Metodo usado para editar um Grupo Musical
+        /// </summary>
+        /// <param name="evento"></param>
+        public void Edit(Evento evento)
+        {
+            _context.Update(evento);
+            _context.SaveChanges();
+
+        }
+
+        public Evento Get(int id)
+        {
+            return _context.Eventos.Find(id);
+        }
+
+        public IEnumerable<Evento> GetAll()
+        {
+            return _context.Eventos.AsNoTracking();
+        }
+    }
+}
