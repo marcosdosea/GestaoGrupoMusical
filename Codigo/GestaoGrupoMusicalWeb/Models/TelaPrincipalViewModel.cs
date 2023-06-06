247651ff066ff1389af695e0512af2dcb4dcba32
matheusmt12 <matheusdacruzsouza1998@gmail.com>
2023-06-06 16:38:03 -0300

    criação e implementação da controller

---
+++ Codigo/GestaoGrupoMusicalWeb/Models/TelaPrincipalViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GestaoGrupoMusicalWeb.Models
+{
+    public class TelaPrincipalViewModel
+    {
+        public IEnumerable<EnsaioViewModelDTO> Ensaio { get; set; }
+        public IEnumerable<EventoViewModelDTO> Evento { get; set; }
+
+    }
+}
