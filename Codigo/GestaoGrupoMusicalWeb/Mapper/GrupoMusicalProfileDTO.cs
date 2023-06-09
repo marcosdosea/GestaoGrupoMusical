a1de7b1b45002cfc3acdd201da7752a521615364
matheusmt12 <matheusdacruzsouza1998@gmail.com>
2023-06-09 11:17:18 -0300

    Feito Mapper do GrupoMusicalDTO

---
+++ Codigo/GestaoGrupoMusicalWeb/Mapper/GrupoMusicalProfileDTO.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core.DTO;
+using GestaoGrupoMusicalWeb.Models;
+
+namespace GestaoGrupoMusicalWeb.Mapper
+{
+    public class GrupoMusicalProfileDTO : Profile
+    {
+        GrupoMusicalProfileDTO()
+        {
+            CreateMap<GrupoMusicalViewModelDTO, GrupoMusicalDTO>().ReverseMap();
+        }
+    }
+}
