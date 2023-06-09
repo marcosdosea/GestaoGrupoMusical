a6dc7942d77db0299f9417d85b3d36aee46fe2bb
Carlos Daniel <ms_danniel@outlook.com>
2023-06-09 01:20:53 -0300

    adicionado metodos get/post para remover adm de grupo musical. #31

--- Codigo/GestaoGrupoMusicalWeb/Controllers/PessoaController.cs
+++ Codigo/GestaoGrupoMusicalWeb/Controllers/PessoaController.cs
@@ -125,13 +125,47 @@ namespace GestaoGrupoMusicalWeb.Controllers
         }
 
         //Administradores do Grupo Musical\\
-
+        /// <summary>
+        /// Este metodo lista todos os administradores de um determinado grupo
+        /// </summary>
+        /// <param name="id">id do grupo o qual queremos ver os administradores</param>
+        /// <returns>lista de administradores</returns>
         public ActionResult IndexAdmGroup(int id)
         {
             var listaPessoas = _pessoaService.GetAllAdmGroup(id);
             var listaPessoasModel = _mapper.Map<List<PessoaViewModel>>(listaPessoas);
 
             return View(listaPessoasModel);
+        }
+
+        /// <summary>
+        /// Remover o posto de administrador de grupo musical
+        /// de algum usuario
+        /// </summary>
+        /// <param name="id">id do usuario alvo</param>
+        /// <returns>view com informações do usuario para confirmar</returns>
+        // GET: PessoaController/RemoveAdmGroup/5
+        public ActionResult RemoveAdmGroup(int id)
+        {
+            var pessoa = _pessoaService.Get(id);
+            var pessoaViewModel = _mapper.Map<PessoaViewModel>(pessoa);
+
+            return View(pessoaViewModel);
+        }
+
+        /// <summary>
+        /// Remove usuario do posto de adm de grupo musical
+        /// </summary>
+        /// <param name="id">id do usuario alvo</param>
+        /// <param name="pessoaViewModel"></param>
+        /// <returns></returns>
+        // POST: PessoaController/RemoveAdmGroup/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RemoveAdmGroup(int id, PessoaViewModel pessoaViewModel)
+        {
+            _pessoaService.RemoveAdmGroup(id);
+            return RedirectToAction(nameof(IndexAdmGroup));
         }
     }
 }
