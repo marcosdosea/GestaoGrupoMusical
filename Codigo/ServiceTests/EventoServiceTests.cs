e7639e14daa172d05266d37c4f03b9002ae7f36a
junio10 <junio.js977@gmail.com>
2023-06-09 11:42:16 -0300

    Feature: criando classe ServiceTests/EventoServiceTests.cs e os metodos dela. E criado a EventoControllerTests.cs #339

---
+++ Codigo/ServiceTests/EventoServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using Core.Service;
+using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServiceTests
+{
+    public class EventoServiceTests
+    {
+        private GrupoMusicalContext _context;
+        private IEvento _evento;
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+        [TestMethod()]
+        public void CreateTest()
+        {
+
+        }
+        [TestMethod()]
+        public void DeleteTest()
+        { 
+
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+
+        }
+        [TestMethod()]
+        public void GetTest()
+        {
+
+        }
+        [TestMethod()]
+        public void GetAllTest()
+        {
+
+        }
+
+    }
+
+}
