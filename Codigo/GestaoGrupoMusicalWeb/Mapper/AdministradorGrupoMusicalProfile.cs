1bb50cd1007f305cd5a0d56c04fbaf75edce5afe
Carlos Daniel <ms_danniel@outlook.com>
2023-06-09 23:12:12 -0300

    mapper para viewmodel de adm de grupo musical

---
+++ Codigo/GestaoGrupoMusicalWeb/Mapper/AdministradorGrupoMusicalProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core;
+using GestaoGrupoMusicalWeb.Models;
+
+namespace GestaoGrupoMusicalWeb.Mapper
+{
+    public class AdministradorGrupoMusicalProfile : Profile
+    {
+        public AdministradorGrupoMusicalProfile()
+        {
+            CreateMap<AdministradorGrupoMusicalViewModel, Pessoa>().ReverseMap();
+        }
+
+    }
+}
