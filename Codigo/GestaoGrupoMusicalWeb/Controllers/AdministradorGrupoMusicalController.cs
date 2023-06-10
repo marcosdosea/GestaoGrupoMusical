376dbb8f0b5a2ee91ddcd663808b54622b1e14b8
Carlos Daniel <ms_danniel@outlook.com>
2023-06-09 23:11:06 -0300

    uso de viewbag para passar informações para outros metodos e views. #31

--- Codigo/GestaoGrupoMusicalWeb/Controllers/AdministradorGrupoMusicalController.cs
+++ Codigo/GestaoGrupoMusicalWeb/Controllers/AdministradorGrupoMusicalController.cs
@@ -23,10 +23,15 @@ namespace GestaoGrupoMusicalWeb.Controllers
         /// Este metodo lista todos os administradores de um determinado grupo
         /// </summary>
         /// <param name="id">id do grupo o qual queremos ver os administradores</param>
+        /// <param name="NomeGrupo">nome do grupo para por na view bag</param>
         /// <returns>lista de administradores</returns>
         public ActionResult Index(int id, string NomeGrupo)
         {
+            //essas informações na viewbag serão utilizados
+            //na view para poder repassar para outros metodos
+            //como o create e delete
             ViewBag.Nome = NomeGrupo;
+            ViewBag.IdGrupo = id;
 
             var listaPessoas = _pessoaService.GetAllAdmGroup(id);
 
@@ -39,17 +44,22 @@ namespace GestaoGrupoMusicalWeb.Controllers
             return View();
         }
 
+        /// <summary>
+        /// </summary>
+        /// <param name="admViewModel">viewmodel de pessoa</param>
+        /// <param name="nomeGrupo">nome do grupo para passar para o index</param>
+        /// <returns></returns>
         // POST: AdministradorGrupoMusicalController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(AdministradorGrupoMusicalViewModel admViewModel)
+        public ActionResult Create(AdministradorGrupoMusicalViewModel admViewModel, string nomeGrupo)
         {
             if (ModelState.IsValid)
             {
                 var pessoa = _mapper.Map<Pessoa>(admViewModel);
                 _pessoaService.AddAdmGroup(pessoa);
             }
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(Index), new { id=admViewModel.IdGrupoMusical, NomeGrupo=nomeGrupo});
         }
 
         /// <summary>
@@ -57,12 +67,16 @@ namespace GestaoGrupoMusicalWeb.Controllers
         /// de algum usuario
         /// </summary>
         /// <param name="id">id do usuario alvo</param>
+        /// <param name="nomeGrupo">nome do grupo para por na view bag</param>
         /// <returns>view com informações do usuario para confirmar</returns>
         // GET: PessoaController/RemoveAdmGroup/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int id, string nomeGrupo)
         {
             var pessoa = _pessoaService.Get(id);
             var pessoaViewModel = _mapper.Map<PessoaViewModel>(pessoa);
+
+            ViewBag.idGrupoMusical = pessoa.IdGrupoMusical;
+            ViewBag.nomeGrupo = nomeGrupo;
 
             return View(pessoaViewModel);
         }
@@ -72,14 +86,15 @@ namespace GestaoGrupoMusicalWeb.Controllers
         /// </summary>
         /// <param name="id">id do usuario alvo</param>
         /// <param name="pessoaViewModel"></param>
+        /// <param name="nomeGrupo">nome do grupo para redirecionar para o index</param>
         /// <returns></returns>
         // POST: PessoaController/RemoveAdmGroup/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, PessoaViewModel pessoaViewModel)
+        public ActionResult Delete(int id, int idGrupoMusical, string nomeGrupo)
         {
             _pessoaService.RemoveAdmGroup(id);
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(Index), new { id= idGrupoMusical, NomeGrupo = nomeGrupo });
         }
     }
 }
