3cceea450d922eb26d941d4e7c97fd934584c7c8
Lauro Santana Silva <devlauross@gmail.com>
2023-06-11 22:00:52 -0300

    Add DTO

---
+++ Codigo/Core/DTO/InstrumentoMusicalDTO.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Core.DTO
+{
+    public class InstrumentoMusicalDTO
+    {
+        public int Id { get; set; }
+
+        public string Patrimonio { get; set; } = string.Empty;
+
+        public string NomeInstrumento { get; set; } = string.Empty;
+
+        public string Status { get; set; } = string.Empty;
+
+        public string NomeAssociado { get; set; } = string.Empty;
+    }
+}
