82bd6515469912b1f9fe7e445003bdcd867bff1e
Moisés Santos <50469065+junio10@users.noreply.github.com>
2023-06-15 17:16:46 -0300

    Update PapelGrupoService.cs
    FIX: Adicionando comentários em Service/PapelGrupoService

--- Codigo/Service/PapelGrupoService.cs
+++ Codigo/Service/PapelGrupoService.cs
@@ -17,7 +17,11 @@ namespace Service
         {
             _context = context;
         }
-
+        /// <summary>
+        /// Método usado a para adicionar um novo Papel Grupo
+        /// </summary>
+        /// <param name="papel"></param>
+        /// <returns>Id do PapelGrupo</returns>
         public int Create(Papelgrupo papel)
         {
             _context.Papelgrupos.Add(papel);
@@ -25,25 +29,38 @@ namespace Service
 
             return papel.IdPapelGrupo;
         }
-
+         /// <summary>
+        /// Método que deleta um Papel Grupo
+        /// </summary>
+        /// <param name="id"></param>
         public void Delete(int id)
         {
             var papel = _context.Papelgrupos.Find(id);
             _context.Remove(papel);
             _context.SaveChanges();
         }
-
+        /// <summary>
+        /// Metodo usado para editar um Papel Grupo
+        /// </summary>
+        /// <param name="papel"></param>
         public void Edit(Papelgrupo papel)
         {
             _context.Update(papel);
             _context.SaveChanges();
         }
-
+        /// <summary>
+        /// Consulta um Papel Grupo no banco de dados
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>O Palpel Grupo correspondente ao id ou um Papel Grupo vazio</returns>
         public Papelgrupo Get(int id)
         {
             return _context.Papelgrupos.Find(id);
         }
-
+        /// <summary>
+        /// Consulta todos os Papel Grupo no banco de dados
+        /// </summary>
+        /// <returns>Uma lista contendo todos os Papel Grupo</returns>
         public IEnumerable<Papelgrupo> GetAll()
         {
             return _context.Papelgrupos.AsNoTracking();
