a8f7d1da90078d284ff13314c644f8ea72f6433b
Moisés Santos <50469065+junio10@users.noreply.github.com>
2023-06-15 17:07:42 -0300

    Update ManequimService.cs
    fix:Adicionando os comentarios na classe.

--- Codigo/Service/ManequimService.cs
+++ Codigo/Service/ManequimService.cs
@@ -17,7 +17,11 @@ namespace Service
         {
             _context = context;
         }
-
+        /// <summary>
+        /// Método usado a para adicionar um novo manequim
+        /// </summary>
+        /// <param name="manequim"></param>
+        /// <returns>Id do Manequim</returns>
         public int Create(Manequim manequim)
         {
             _context.Manequims.Add(manequim);
@@ -25,25 +29,38 @@ namespace Service
 
             return manequim.Id;
         }
-
+        /// <summary>
+        /// Método que deleta um manequim
+        /// </summary>
+        /// <param name="id"></param>
         public void Delete(int id)
         {
             var manequim = _context.Manequims.Find(id);
             _context.Remove(manequim);
             _context.SaveChanges();
         }
-
+        /// <summary>
+        /// Metodo usado para editar um manequim
+        /// </summary>
+        /// <param name="manequim"></param>
         public void Edit(Manequim manequim)
         {
             _context.Update(manequim);
             _context.SaveChanges();
         }
-
+        /// <summary>
+        /// Consulta um Manequim no banco de dados
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>O Manequim correspondente ao id ou um Manequim vazio</returns>
         public Manequim Get(int id)
         {
             return _context.Manequims.Find(id);
         }
-
+        /// <summary>
+        /// Consulta todos os Manequins no banco de dados
+        /// </summary>
+        /// <returns>Uma lista contendo todos os Manequins</returns>
         public IEnumerable<Manequim> GetAll()
         {
             return _context.Manequims.AsNoTracking();
