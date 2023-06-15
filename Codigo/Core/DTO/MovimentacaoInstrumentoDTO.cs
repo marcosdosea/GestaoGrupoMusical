ac636138b51d2990be72f12ba60d2556b1ad82ea
Lauro Santana Silva <devlauross@gmail.com>
2023-06-15 20:22:45 -0300

    Add DTO Movimentacao

---
+++ Codigo/Core/DTO/MovimentacaoInstrumentoDTO.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.DTO
+{
+    public class MovimentacaoInstrumentoDTO
+    {
+        [Display (Name = "CPF")]
+        public string Cpf { get; set; } = string.Empty;
+
+        [Display(Name = "Associado")]
+        public string NomeAssociado { get; set; } = string.Empty;
+
+        public DateTime Data { get; set; }
+
+        [Display(Name = "Movimentação")]
+        public string Movimentacao { get; set; } = string.Empty;
+
+        public string Status { get; set; } = string.Empty;
+    }
+}
