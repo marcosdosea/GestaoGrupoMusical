3e85c81596de23fd23f612c12b47628b533e0750
junio10 <junio.js977@gmail.com>
2023-06-16 10:23:19 -0300

    fix: Adicionando displays no EventoViewModel. E apagando em Views/Evento/Create.cshtml e Views/Evento/Edit.cshtml os labels colocado. #55

--- Codigo/GestaoGrupoMusicalWeb/Models/EventoViewModel.cs
+++ Codigo/GestaoGrupoMusicalWeb/Models/EventoViewModel.cs
@@ -7,15 +7,24 @@ namespace GestaoGrupoMusicalWeb.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Display(Name = "Grupo Musical")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public int IdGrupoMusical { get; set; }
-        [Required]
+        [Display(Name = "Data Hora Início")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public DateTime DataHoraInicio { get; set; }
-        [Required]
+        [Display(Name = "Data Hora Fim")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public DateTime DataHoraFim { get; set; }
+        [Display(Name = "Local")]
         public string? Local { get; set; }
+        [Display(Name = "Repetório")]
         public string? Repertorio { get; set; }
+        [Display(Name = "Colaborador Reponsável")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public int IdColaboradorResponsavel { get; set; }
+        [Display(Name = "Regente")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public int IdRegente { get; set; }
 
         public SelectList? ListaPessoa { get; set; }
