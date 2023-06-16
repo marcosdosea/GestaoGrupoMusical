ef5d22e68cddc065418ba901ca424b33bd06f36c
Lauro Santana Silva <devlauross@gmail.com>
2023-06-16 11:21:44 -0300

    Add modulo de e-mail

---
+++ Codigo/Email/EmailService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace Email
+{
+    public class EmailService
+    {
+        public static async Task<bool> Enviar(EmailModel mailModel)
+        {
+            if (mailModel.To.Count == 0)
+            {
+                return false;
+            }
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress(mailModel.From, mailModel.Assunto);
+            foreach (var t in mailModel.To)
+            {
+                mail.To.Add(t);
+            }
+
+            mail.Subject = mailModel.Subject;
+            mail.Body = mailModel.Body;
+            mail.IsBodyHtml = true;
+
+            using (SmtpClient smtp = new())
+            {
+
+                smtp.Host = mailModel.Host;
+                smtp.Credentials = new NetworkCredential(mailModel.UserName, mailModel.Password);
+                smtp.EnableSsl = true;
+                smtp.Port = mailModel.Port;
+
+
+                await smtp.SendMailAsync(mail);
+
+                return true;
+            }
+        }
+    }
+}
