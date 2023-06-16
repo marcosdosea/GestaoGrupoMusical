bae5d3930f3a957f7ffaf49d3bebd3361335655f
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2023-06-16 19:18:30 -0300

    Inicialização dos atributos necessários para a realização dos testes. #77

---
+++ Codigo/ServiceTests/PapelGrupoServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using Core.Service;
+using Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Service.Tests
+{
+    [TestClass]
+    public class PapelGrupoServiceTests
+    {
+        private GrupoMusicalContext _context;
+        private IPapelGrupo _papelGrupo;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            //Arrange
+            var builder = new DbContextOptionsBuilder<GrupoMusicalContext>();
+            builder.UseInMemoryDatabase("GrupoMusical");
+            var options = builder.Options;
+
+            _context = new GrupoMusicalContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            var PapeisGrupo = new List<Papelgrupo>
+            {
+                new Papelgrupo
+                {
+                    IdPapelGrupo = 1,
+                    Nome = "COLABORADOR"
+                },
+                new Papelgrupo
+                {
+                    IdPapelGrupo = 2,
+                    Nome = "ADMINISTRADOR GRUPO"
+                },
+                new Papelgrupo
+                {
+                    IdPapelGrupo = 3,
+                    Nome = "ADMINISTRADOR SISTEMA"
+                }
+            };
+
+            _context.AddRange(PapeisGrupo);
+            _context.SaveChanges();
+
+            _papelGrupo = new PapelGrupoService(_context);
+        }
+
+    }
+}
