3d32383a4c71cfff2b660a6b758e65fbade179bc
Carlos Daniel <ms_danniel@outlook.com>
2023-06-16 21:38:42 -0300

    update

---
+++ Codigo/Core/Service/IEventoService.cs
@@ -0,0 +1,14 @@
+﻿using Core.DTO;
+
+namespace Core.Service
+{
+    public interface IEventoService
+    {
+        int Create(Evento evento);
+        void Edit(Evento evento);
+        void Delete(int id);
+        Evento Get(int id);
+        IEnumerable<Evento> GetAll();
+        IEnumerable<EventoDTO> GetAllDTO();
+    }
+}
---
+++ Codigo/Core/Service/IGrupoMusical.cs
@@ -0,0 +1,19 @@
+﻿using Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IGrupoMusical
+    {
+        int Create(Grupomusical grupomusical);
+        void Edit(Grupomusical grupomusical);
+        void Delete(int id);
+        Grupomusical Get(int id);
+        IEnumerable<Grupomusical> GetAll();
+        IEnumerable<GrupoMusicalDTO> GetAllDTO();
+    }
+}
