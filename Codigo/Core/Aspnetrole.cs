56b7076c25b346a778f0ac82657ec94a9306ccb3
Lauro Santana Silva <devlauross@gmail.com>
2023-06-21 23:21:51 -0300

    Add tabelas identity e Informativo

---
+++ Codigo/Core/Aspnetrole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public partial class Aspnetrole
+    {
+        public Aspnetrole()
+        {
+            Aspnetroleclaims = new HashSet<Aspnetroleclaim>();
+            Users = new HashSet<Aspnetuser>();
+        }
+
+        public string Id { get; set; } = null!;
+        public string? Name { get; set; }
+        public string? NormalizedName { get; set; }
+        public string? ConcurrencyStamp { get; set; }
+
+        public virtual ICollection<Aspnetroleclaim> Aspnetroleclaims { get; set; }
+
+        public virtual ICollection<Aspnetuser> Users { get; set; }
+    }
+}
