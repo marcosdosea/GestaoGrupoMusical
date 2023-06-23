7ada345989753589c3d9d22cdf8be92a237dd7fa
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2023-06-22 22:46:57 -0300

    Criação do PessoaDTO. #113

---
+++ Codigo/Core/DTO/PessoaDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    public class PessoaDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = null!;
+        public sbyte Ativo { get; set; }
+    }
+}
