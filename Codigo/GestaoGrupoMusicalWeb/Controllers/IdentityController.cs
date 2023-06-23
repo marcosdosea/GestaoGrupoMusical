b558365766dd2c67fbd0f0744f40cddebbbc5999
Lauro Santana Silva <devlauross@gmail.com>
2023-06-22 23:15:57 -0300

    Add Identity Controller e ViewModel

---
+++ Codigo/GestaoGrupoMusicalWeb/Controllers/IdentityController.cs
@@ -0,0 +1,27 @@
+﻿using Core;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using static GestaoGrupoMusicalWeb.Models.IdentityViewModel;
+
+namespace GestaoGrupoMusicalWeb.Controllers
+{
+    public class IdentityController : Controller
+    {
+        private readonly SignInManager<UsuarioIdentity> _signInManager;
+
+        [HttpGet]
+        public async Task<IActionResult> Autenticar()
+        {
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Autenticar(AutenticarViewModel model)
+        {
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+            return View();
+        }
+    }
+}
---
+++ Codigo/GestaoGrupoMusicalWeb/Models/IdentityViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoGrupoMusicalWeb.Models
+{
+    public class IdentityViewModel
+    {
+        public class AutenticarViewModel
+        {
+            [Required]
+            public string Cpf { get; set; } = string.Empty;
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Senha { get; set; } = string.Empty;
+        }
+    }
+}
