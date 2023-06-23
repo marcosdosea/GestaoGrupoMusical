35d947b45a07abb29e4c147c266d1558c6e49030
Lauro Santana Silva <devlauross@gmail.com>
2023-06-23 18:43:48 -0300

    Pagina inicial setada para ser Autenticar

--- Codigo/GestaoGrupoMusicalWeb/Program.cs
+++ Codigo/GestaoGrupoMusicalWeb/Program.cs
@@ -92,7 +92,7 @@ namespace GestaoGrupoMusicalWeb
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Identity}/{action=Autenticar}/{id?}");
 
             app.Run();
         }
