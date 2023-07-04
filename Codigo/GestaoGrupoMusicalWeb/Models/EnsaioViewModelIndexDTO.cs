8abfa75639f978bba8bb221895b93c90a011ca96
junio10 <junio.js977@gmail.com>
2023-07-04 10:04:35 -0300

    feat: Adicionando uma viewModel para o DTO

---
+++ Codigo/GestaoGrupoMusicalWeb/Models/EnsaioViewModelIndexDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace GestaoGrupoMusicalWeb.Models
+{
+    public class EnsaioViewModelIndexDTO
+    {
+        public int Id { get; set; }
+        public DateTime? DataHoraInicio { get; set; }
+        public string Tipo { get; set; } = null!;
+        public string? Local { get; set; }
+        public sbyte PresencaObrigatoria { get; set; }
+    }
+}
