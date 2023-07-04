4384f6d355542d4f472fa78b6feebce7ac0f9795
Reinan <reinanzinho100@gmail.com>
2023-07-04 00:01:31 -0300

    estilização da home/index com o manter informativo adicionado. resolve #157

--- Codigo/GestaoGrupoMusicalWeb/Models/InformativoViewModel.cs
+++ Codigo/GestaoGrupoMusicalWeb/Models/InformativoViewModel.cs
@@ -1,10 +1,18 @@
-﻿namespace GestaoGrupoMusicalWeb.Models
+﻿using MessagePack;
+using Microsoft.AspNetCore.Cors;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestaoGrupoMusicalWeb.Models
 {
     public class InformativoViewModel
     {
         public int IdGrupoMusical { get; set; }
         public int IdPessoa { get; set; }
+        [Display(Name ="Informativo")]
+        [MaxLength(2000, ErrorMessage = "Ultrapassou o limíte de 2000 caracteres")]
         public string Mensagem { get; set; } = null!;
+        [Display(Name = "Data")]
+        [Required(ErrorMessage = "campo {0} é obrigatório")]
         public DateTime Data { get; set; }
         public sbyte EntregarAssociadosAtivos { get; set; }
     }
