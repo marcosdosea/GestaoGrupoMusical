9fcd880533bc0780d630505405b2581002154bce
junio10 <junio.js977@gmail.com>
2023-07-04 10:07:35 -0300

    feat: Adicioando Mapper/EnsaioProfileIndexDTO.cs

---
+++ Codigo/GestaoGrupoMusicalWeb/Mapper/EnsaioProfileIndexDTO.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core.DTO;
+using GestaoGrupoMusicalWeb.Models;
+
+namespace GestaoGrupoMusicalWeb.Mapper
+{
+    public class EnsaioProfileIndexDTO: Profile
+    {
+        public EnsaioProfileIndexDTO()
+        {
+            CreateMap<EnsaioViewModelIndexDTO, EnsaioIndexDTO>().ReverseMap();
+        }
+    }
+}
