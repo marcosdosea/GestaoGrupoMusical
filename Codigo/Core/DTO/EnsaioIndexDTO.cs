9f568364888adff5e854cc4112ff6dce69374423
junio10 <junio.js977@gmail.com>
2023-07-05 13:03:22 -0300

    feat: adicionando o displayName no DTO/EnsaioIndexDTO.cs

--- Codigo/Core/DTO/EnsaioIndexDTO.cs
+++ Codigo/Core/DTO/EnsaioIndexDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,13 @@ namespace Core.DTO
     public class EnsaioIndexDTO
     {
         public int Id { get; set; }
+        [DisplayName("Data Hora Inicio")]
         public DateTime? DataHoraInicio { get; set; }
+        [DisplayName("Tipo")]
         public string Tipo { get; set; } = null!;
+        [DisplayName("Local")]
         public string? Local { get; set; }
+        [DisplayName("Presença Obrigatória")]
         public sbyte PresencaObrigatoria { get; set; }
     }
 }
