5e6aae3d97022ef50367c1752581cf01ba38d7f3
matheus <matheusdacruzsouza1998@gmail.com>
2023-07-05 16:57:57 -0300

    Criação da classe de teste de ColaboradorController

---
+++ Codigo/GestaoGrupoMusicalWebTests/Controllers/ColaboradorControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GestaoGrupoMusicalWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoGrupoMusicalWeb.Controllers.Tests
+{
+    [TestClass()]
+    public class ColaboradorControllerTests
+    {
+        [TestMethod()]
+        public void ColaboradorControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
