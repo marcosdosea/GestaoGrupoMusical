ee7fb54e8bab88f307cfa79cc69c554a64531514
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2023-07-11 03:04:14 -0300

    Adição do método Initialize(). #185

--- Codigo/ServiceTests/MovimentacaoInstrumentoServiceTest.cs
+++ Codigo/ServiceTests/MovimentacaoInstrumentoServiceTest.cs
@@ -1,6 +1,9 @@
 ﻿using Core;
 using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,5 +17,56 @@ namespace ServiceTests
     {
         private GrupoMusicalContext _context;
         private IMovimentacaoInstrumentoService _movimentacaoInstrumento;
+
+        [TestInitialize]
+        private void Initialize()
+        {
+            // Arrange
+            var builder = new DbContextOptionsBuilder<GrupoMusicalContext>();
+            builder.UseInMemoryDatabase("GrupoMusical");
+            var options = builder.Options;
+
+            _context = new GrupoMusicalContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            var movimentacoesInstrumentos = new List<Movimentacaoinstrumento>
+            {
+                new Movimentacaoinstrumento
+                {
+                    Id = 1,
+                    Data = new DateTime(2022, 8, 3),
+                    IdInstrumentoMusical = 1,
+                    IdAssociado = 1,
+                    IdColaborador = 1,
+                    ConfirmacaoAssociado = 1,
+                    TipoMovimento = "EMPRESTIMO"
+                },
+                new Movimentacaoinstrumento
+                {
+                    Id = 2,
+                    Data = new DateTime(2023, 7, 5),
+                    IdInstrumentoMusical = 2,
+                    IdAssociado = 2,
+                    IdColaborador = 1,
+                    ConfirmacaoAssociado = 2,
+                    TipoMovimento = "DEVOLUCAO"
+                },
+                new Movimentacaoinstrumento
+                {
+                    Id = 3,
+                    Data = new DateTime(2023, 2, 28),
+                    IdInstrumentoMusical = 3,
+                    IdAssociado = 3,
+                    IdColaborador = 2,
+                    ConfirmacaoAssociado = 1,
+                    TipoMovimento = "EMPRESTIMO"
+                }
+            };
+
+            _context.AddRange(movimentacoesInstrumentos);
+            _context.SaveChanges();
+
+            _movimentacaoInstrumento = new MovimentacaoInstrumentoService(_context);
+        }
     }
 }
