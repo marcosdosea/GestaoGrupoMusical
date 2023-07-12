bafede04d62ae2ed55ba20af41da5a74cf28e2b7
Carlos Daniel <ms_danniel@outlook.com>
2023-07-12 16:25:32 -0300

    autorized

--- Codigo/GestaoGrupoMusicalWeb/Controllers/HomeController.cs
+++ Codigo/GestaoGrupoMusicalWeb/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using AutoMapper;
 using Core.Service;
 using GestaoGrupoMusicalWeb.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace GestaoGrupoMusicalWeb.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
