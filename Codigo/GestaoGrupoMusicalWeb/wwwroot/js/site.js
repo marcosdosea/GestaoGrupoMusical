6da635103cf3f30638259f5a196bb405b421e6d0
Carlos Daniel <ms_danniel@outlook.com>
2023-07-12 16:26:10 -0300

    script para ver/esconder senha

--- Codigo/GestaoGrupoMusicalWeb/wwwroot/js/site.js
+++ Codigo/GestaoGrupoMusicalWeb/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+<script>
+    const toggleSenha = document.querySelector('#toggleSenha');
+    const senhaInput = document.querySelector('#senha');
+
+    toggleSenha.addEventListener('click', function () {
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+    toggleSenha.classList.remove('fa-eye');
+    toggleSenha.classList.add('fa-eye-slash');
+    } else {
+        senhaInput.type = 'password';
+    toggleSenha.classList.remove('fa-eye-slash');
+    toggleSenha.classList.add('fa-eye');
+    }
+  });
+</script>
+
