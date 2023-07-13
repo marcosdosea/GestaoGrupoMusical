13db39474b3981c8146072859355075dcd256019
Carlos Daniel <ms_danniel@outlook.com>
2023-07-13 11:08:25 -0300

    removida tag

--- Codigo/GestaoGrupoMusicalWeb/wwwroot/js/site.js
+++ Codigo/GestaoGrupoMusicalWeb/wwwroot/js/site.js
@@ -3,21 +3,19 @@
 
 // Write your JavaScript code.
 
+const toggleSenha = document.querySelector('#toggleSenha');
+const senhaInput = document.querySelector('#InputSenha');
 
-<script>
-    const toggleSenha = document.querySelector('#toggleSenha');
-    const senhaInput = document.querySelector('#senha');
-
-    toggleSenha.addEventListener('click', function () {
-    if (senhaInput.type === 'password') {
-        senhaInput.type = 'text';
+toggleSenha.addEventListener('click', function () {
+if (senhaInput.type === 'password') {
+    senhaInput.type = 'text';
     toggleSenha.classList.remove('fa-eye');
     toggleSenha.classList.add('fa-eye-slash');
-    } else {
-        senhaInput.type = 'password';
+} else {
+    senhaInput.type = 'password';
     toggleSenha.classList.remove('fa-eye-slash');
     toggleSenha.classList.add('fa-eye');
-    }
-  });
-</script>
+}
+});
+
 
