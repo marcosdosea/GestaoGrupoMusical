b41125c704ac48522788a5a68398cee01679cddf
junio10 <junio.js977@gmail.com>
2023-07-19 17:22:53 -0300

    refactor: colocando notificacao na Controllers/EnsaioController.cs no Delete

--- Codigo/GestaoGrupoMusicalWeb/Controllers/EnsaioController.cs
+++ Codigo/GestaoGrupoMusicalWeb/Controllers/EnsaioController.cs
@@ -56,7 +56,7 @@ namespace GestaoGrupoMusicalWeb.Controllers
             ensaioViewModel.ListaGrupoMusical = new SelectList(_grupoMusical.GetAll(), "Id", "Nome");
             if (ModelState.IsValid)
             {
-                string mensagem = string.Empty;
+                String mensagem = String.Empty;
                 switch(await _ensaio.Create(_mapper.Map<Ensaio>(ensaioViewModel)))
                 {
                     case 200:
@@ -68,7 +68,7 @@ namespace GestaoGrupoMusicalWeb.Controllers
                         Notificar(mensagem, Notifica.Alerta);
                         return View("Create", ensaioViewModel);
                     case 401:
-                        mensagem = "Alerta ! A <b>data de início</b> deve ser maior que a data de hoje " + DateTime.Now.ToString();
+                        mensagem = "Alerta ! A <b>data de início</b> deve ser maior que a data de hoje " + DateTime.Now;
                         Notificar(mensagem, Notifica.Alerta);
                         return View("Create", ensaioViewModel);
                     case 500:
@@ -103,25 +103,25 @@ namespace GestaoGrupoMusicalWeb.Controllers
             ensaioViewModel.ListaGrupoMusical = new SelectList(_grupoMusical.GetAll(), "Id", "Nome");
             if (ModelState.IsValid)
             {
-                string mensagem = string.Empty;
-                switch (await _ensaio.Create(_mapper.Map<Ensaio>(ensaioViewModel)))
+                String mensagem = String.Empty;
+                switch (await _ensaio.Edit(_mapper.Map<Ensaio>(ensaioViewModel)))
                 {
                     case 200:
                         mensagem = "Ensaio <b>Editado</b> com <b>Sucesso</b>";
                         Notificar(mensagem, Notifica.Sucesso);
                         return RedirectToAction(nameof(Index));
-                    case 400:
-                        mensagem = "Alerta ! A <b>data de início</b> deve ser menor que a data de <b>fim</b>";
-                        Notificar(mensagem, Notifica.Alerta);
-                        return View("Create", ensaioViewModel);
                     case 401:
-                        mensagem = "Alerta ! A <b>data de início</b> deve ser maior que a data de hoje " + DateTime.Now.ToString();
+                        mensagem = "Alerta ! A <b>data de início</b> deve ser menor que a data de <b>a data de fim</b>, ou a <b>a data de fim</b> tem que ser maior";
                         Notificar(mensagem, Notifica.Alerta);
-                        return View("Create", ensaioViewModel);
+                        return View("Edit", ensaioViewModel);
+                    case 400:
+                        mensagem = "Alerta ! A <b>data de início</b> deve ser maior que a data de hoje " + DateTime.Now;
+                        Notificar(mensagem, Notifica.Alerta);
+                        return View("Edit", ensaioViewModel);
                     case 500:
                         mensagem = "<b>Erro</b> ! Desculpe, ocorreu um erro durante o <b>Editar</b> de ensaio, se isso persistir entre em contato com o suporte";
                         Notificar(mensagem, Notifica.Erro);
-                        return RedirectToAction("Create", ensaioViewModel);
+                        return RedirectToAction("Edit", ensaioViewModel);
 
 
                 }
@@ -141,7 +141,19 @@ namespace GestaoGrupoMusicalWeb.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Delete(EnsaioViewModel ensaioModel)
         {
-            await _ensaio.Delete(ensaioModel.Id);
+            String mensagem = String.Empty;
+            switch (await _ensaio.Delete(ensaioModel.Id))
+            {
+                case 200:
+                    mensagem = "Ensaio <b>Deletado</b> com <b>Sucesso</b>";
+                    Notificar(mensagem, Notifica.Sucesso);
+                    return RedirectToAction(nameof(Index));
+                case 500:
+                    mensagem = "<b>Erro</b> ! Desculpe, ocorreu um erro durante ao <b>Excluir</b> um ensaio, se isso persistir entre em contato com o suporte";
+                    Notificar(mensagem, Notifica.Erro);
+                    return RedirectToAction(nameof(Index));
+
+            }
             return RedirectToAction(nameof(Index));
         }
     }
