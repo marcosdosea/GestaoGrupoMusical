c667fb6c205ed15edcf796df3537b9470aaf73b3
junio10 <junio.js977@gmail.com>
2023-07-19 16:00:45 -0300

    comentarios colocado na Service/IEnsaioService.cs

--- Codigo/Core/Service/IEnsaioService.cs
+++ Codigo/Core/Service/IEnsaioService.cs
@@ -4,7 +4,27 @@ namespace Core.Service
 {
     public interface IEnsaioService
     {
+        /// <summary>
+        /// Cadastra uma ensaio no banco de dados
+        /// </summary>
+        /// <param name="pessoa"></param>
+        /// <returns>
+        /// 200 - Sucesso <para />
+        /// 400 - Data de inicio fora do escopo,ou seja, é menor que a data de hoje <para />
+        /// 401 - Data de inicio fora do escopo, ou seja, ou seja a data inicio passa da data fim do evento<para />
+        /// 500 - Erro interno
+        /// </returns>
         Task<int> Create(Ensaio ensaio);
+        /// <summary>
+        /// Editar uma ensaio no banco de dados
+        /// </summary>
+        /// <param name="pessoa"></param>
+        /// <returns>
+        /// 200 - Sucesso <para />
+        /// 400 - Data de inicio fora do escopo,ou seja, é menor que a data de hoje <para />
+        /// 401 - Data de inicio fora do escopo, ou seja, ou seja a data inicio passa da data fim do evento<para />
+        /// 500 - Erro interno
+        /// </returns>
         Task<int> Edit(Ensaio ensaio);
         Task<bool> Delete(int id);
         Task<Ensaio> Get(int id);
