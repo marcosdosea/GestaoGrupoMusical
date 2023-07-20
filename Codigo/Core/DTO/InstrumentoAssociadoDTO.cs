cd7d36be9deab6d21f76d198a5cb5ecd34c3eaee
Lauro Santana Silva <devlauross@gmail.com>
2023-07-20 18:35:18 -0300

    Add DTO para confirmarInstrumento

---
+++ Codigo/Core/DTO/InstrumentoAssociadoDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.DTO
+{
+    public class InstrumentoAssociadoDTO
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Instrumento")]
+        public string NomeInstrumento { get; set; } = string.Empty;
+
+        public DateTime Data { get; set; }
+
+        [Display(Name = "Movimentação")]
+        public string Movimentacao { get; set; } = string.Empty;
+
+        public string NomeStatus { get; set; } = string.Empty;
+
+        public bool Status { get; set; }
+    }
+}
