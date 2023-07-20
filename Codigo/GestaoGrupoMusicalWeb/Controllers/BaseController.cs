ad117a12564196c9046657083a73bc0138cb3855
Carlos Daniel <ms_danniel@outlook.com>
2023-07-20 08:34:47 -0300

    metodo para gerar token e enviar email para redefinir senha #251

--- Codigo/GestaoGrupoMusicalWeb/Controllers/BaseController.cs
+++ Codigo/GestaoGrupoMusicalWeb/Controllers/BaseController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core;
+using Email;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GestaoGrupoMusicalWeb.Controllers
 {
@@ -32,6 +35,49 @@ namespace GestaoGrupoMusicalWeb.Controllers
                 break;
             }
             TempData["alertText"] = mensagem;
+        }
+
+        private async Task<int> RequestPasswordReset(UserManager<UsuarioIdentity> _userManager, string userEmail)
+        {
+            var user = await _userManager.FindByEmailAsync(userEmail);
+
+            //a segunda condição é para caso seja necessario
+            //confirmar o email do usuario para alterar a senha
+            if (user == null /*|| !(await _userManager.IsEmailConfirmedAsync(user))*/)
+            {
+                //usuario não encontrado
+                return 400;
+            }
+
+            try
+            {
+                //gera o token para redefinir senha
+                string code = await _userManager.GeneratePasswordResetTokenAsync(user);
+            }
+            catch(Exception ex)
+            {
+                //ero na geração do token
+                return 500;
+            }
+
+            //gera link para a view da controladora ja passando codigo e id do usuario
+            var callbackUrl = Url.Action("ResetPassword", "Identity", new { userId = user.Id, token = code }, /*protocol:*/ Request.Scheme);
+
+            //enviar email com o link
+            EmailModel email = new()
+            {
+                Assunto = "Batalá - Redefinição de Senha",
+                Body = "<div style=\"text-align: center;\">\r\n    " +
+                "<h1>Redefinição de Senha</h1>\r\n    " +
+                $"<h2>Olá, aqui está o link para redefinir sua senha:</h2>\r\n" +
+                $"<a href=\"{callbackUrl}\" style=\"font-weight: 600;\">Clique Aqui</a>"
+            };
+
+            email.To.Add(userEmail);
+
+            await EmailService.Enviar(email);
+
+            return 200;
         }
     }
 }
