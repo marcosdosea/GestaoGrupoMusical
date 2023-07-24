b130e247b0ae4ddff3316ad54eef4ce9620822d2
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2023-07-24 19:24:59 -0300

    Novo nome para o campo referente ao atributo Status: Situação -> Status. Também foi adicionado uma nova div com a classe container, ela é responsável por engloba a maior parte do html. #271

--- Codigo/GestaoGrupoMusicalWeb/Models/InstrumentoMusicalViewModel.cs
+++ Codigo/GestaoGrupoMusicalWeb/Models/InstrumentoMusicalViewModel.cs
@@ -20,7 +20,7 @@ namespace GestaoGrupoMusicalWeb.Models
         public DateTime DataAquisicao { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [Display(Name = "Situação")]
+        [Display(Name = "Status")]
         public string Status { get; set; } = "DISPONIVEL";
 
         [Display(Name = "Instrumento")]
