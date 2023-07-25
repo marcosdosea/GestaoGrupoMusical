6d744d768634bb384a3598384af4b780e3a7334b
Lauro Santana Silva <devlauross@gmail.com>
2023-07-25 15:24:41 -0300

    Testes para add userName

---
+++ Codigo/GestaoGrupoMusicalWeb/Helpers/ApplicationUserClaims.cs
@@ -0,0 +1,26 @@
+﻿using Core;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+
+namespace GestaoGrupoMusicalWeb.Helpers
+{
+    public class ApplicationUserClaims : UserClaimsPrincipalFactory<UsuarioIdentity, IdentityRole>
+    {
+        public ApplicationUserClaims(
+            UserManager<UsuarioIdentity> userManager, 
+            RoleManager<IdentityRole> roleManager,
+            IOptions<IdentityOptions> options)
+            : base(userManager, roleManager, options)
+        {
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(UsuarioIdentity user)
+        {
+            var identity = await base.GenerateClaimsAsync(user);
+            identity.AddClaim(new Claim("UserName","Teste"));
+
+            return identity;
+        }
+    }
+}
