bcaa4f570f39fbd01eca614304156de97c2e36e5
Lauro Santana Silva <devlauross@gmail.com>
2023-07-26 21:16:39 -0300

    Mudança no tipo de retorno do Delete

--- Codigo/Core/Service/IInstrumentoMusicalService.cs
+++ Codigo/Core/Service/IInstrumentoMusicalService.cs
@@ -6,7 +6,7 @@ namespace Core.Service
     {
         Task<int> Create(Instrumentomusical instrumentoMusical);
         Task<int> Edit(Instrumentomusical instrumentoMusical);
-        Task Delete(int id);
+        Task<int> Delete(int id);
         Task<Instrumentomusical?> Get(int id);
         Task<IEnumerable<Instrumentomusical>> GetAll();
         Task<IEnumerable<InstrumentoMusicalDTO>> GetAllDTO(int idGrupo);
