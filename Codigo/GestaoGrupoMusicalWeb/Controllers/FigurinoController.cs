06611756d2c20f3e4fb37aa779f02caa34409a90
Carlos Daniel <ms_danniel@outlook.com>
2023-08-02 11:31:49 -0300

    geração da controller e implementado index. #347

---
+++ Codigo/GestaoGrupoMusicalWeb/Controllers/FigurinoController.cs
@@ -0,0 +1,107 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace GestaoGrupoMusicalWeb.Controllers
+{
+    [Authorize(Roles = "ADMINISTRADOR GRUPO")]
+    public class FigurinoController : BaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IGrupoMusicalService _grupoMusical;
+        private readonly IManequimService _manequim;
+        private readonly IFigurinoService _figurino;
+        private readonly UserManager<UsuarioIdentity> _userManager;
+
+        public FigurinoController(IMapper mapper, IGrupoMusicalService grupoMusical,
+            IManequimService manequim, IFigurinoService figurino, UserManager<UsuarioIdentity> userManager)
+        {
+            _mapper = mapper;
+            _grupoMusical = grupoMusical;
+            _manequim = manequim;
+            _figurino = figurino;
+            _userManager = userManager;
+        }
+
+        // GET: FigurinoController
+        public async Task<ActionResult> Index()
+        {
+            var listFigurinos = await _figurino.GetAll(User.Identity.Name);
+            return View(listFigurinos);
+        }
+
+        // GET: FigurinoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FigurinoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FigurinoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FigurinoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FigurinoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FigurinoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FigurinoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
