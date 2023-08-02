4482e6bf1f097ae9704af47217eab2d34a7b5e62
Carlos Daniel <ms_danniel@outlook.com>
2023-08-02 13:37:12 -0300

    view model. #347

--- Codigo/GestaoGrupoMusicalWeb/Models/FigurinoViewModel.cs
+++ Codigo/GestaoGrupoMusicalWeb/Models/FigurinoViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace GestaoGrupoMusicalWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace GestaoGrupoMusicalWeb.Models
 {
     public class FigurinoViewModel
     {
+        [Required(ErrorMessage = "O código do associado é obrigatótio.")]
+        [Key]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome", Prompt = "Ex.: São João 2023")]
+        [Required(ErrorMessage = "O campo Nome é obrigatótio.")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "O nome do figurino deve ter entre 5 e 100 caracteres")]
+        public string? Name { get; set; }
+
+        [Display(Name = "Data")]
+        [DataType(DataType.Date, ErrorMessage = "É necessário escolher uma data válida.")
+        [Required(ErrorMessage = "O campo Data é obrigatótio.")]
+        public DateTime? dateTime { get; set; }
+
     }
 }
