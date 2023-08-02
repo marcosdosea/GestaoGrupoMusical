a967b9d257486a6b1d8d9e16d5056a6e4aacdcf9
Carlos Daniel <ms_danniel@outlook.com>
2023-08-02 09:22:54 -0300

    service para figurino. #347

---
+++ Codigo/Core/Service/IFigurinoService.cs
@@ -0,0 +1,18 @@
+﻿using Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IFigurinoService
+    {
+        Task<int> Create(FigurinoDTO figurino);
+        Task<int> Edit(FigurinoDTO figurino);
+        Task<int> Delete(int id);
+        FigurinoDTO Get(int id);
+        Task<IEnumerable<FigurinoDTO>> GetAll();
+    }
+}
