b8b0b8388fd7783148f3fee112a33e851cb16b2c
Carlos Daniel <ms_danniel@outlook.com>
2023-08-02 09:23:17 -0300

    dto para figurino + figurinomanequim. #347

---
+++ Codigo/Core/DTO/FigurinoDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    public class FigurinoDTO
+    {
+
+        public int IdFigurino { get; set; }
+        public int IdManequim { get; set; }
+        public int IdGrupoMusical { get; set; }
+        public string Nome { get; set; } = null!;
+        public DateTime? Data { get; set; }
+        public int QuantidadeDisponivel { get; set; }
+        public int QuantidadeEntregue { get; set; }
+
+    }
+}
