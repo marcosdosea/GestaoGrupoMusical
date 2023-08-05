e4375fba0e3fd87bae105d0253faf1ad6db0d839
Carlos Daniel <ms_danniel@outlook.com>
2023-08-05 11:15:00 -0300

    service. #360

---
+++ Codigo/Service/MovimentacaoFigurinoService.cs
@@ -0,0 +1,45 @@
+﻿using Core;
+using Core.DTO;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class MovimentacaoFigurinoService : IMovimentacaoFigurinoService
+    {
+        private readonly GrupoMusicalContext _context;
+
+        public MovimentacaoFigurinoService(GrupoMusicalContext context)
+        {
+            _context = context;
+        }
+
+        public Task<int> CreateAsync(Movimentacaofigurino movimentacao)
+        {
+            using var transaction = _context.Database.BeginTransaction();
+
+
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<MovimentacaoFigurinoDTO>> GetAllByIdInstrumento(int idFigurino)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movimentacaofigurino?> GetEmprestimoByIdInstrumento(int idFigurino)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
