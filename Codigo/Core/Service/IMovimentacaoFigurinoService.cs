06bc6c205e4b9542a52aa22372e47fcd85836a50
Carlos Daniel <ms_danniel@outlook.com>
2023-08-05 11:04:08 -0300

    criado metodos. #360

--- Codigo/Core/Service/IMovimentacaoFigurinoService.cs
+++ Codigo/Core/Service/IMovimentacaoFigurinoService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,28 @@ namespace Core.Service
 {
     public interface IMovimentacaoFigurinoService
     {
+        /// <summary>
+        /// Cadastra uma movimentação no banco de dados
+        /// </summary>
+        /// <param name="movimentacao"></param>
+        /// <returns>
+        /// 200 - Sucesso
+        /// 500 - Erro interno
+        /// </returns>
+        Task<int> CreateAsync(Movimentacaofigurino movimentacao);
 
+        Task<Movimentacaofigurino?> GetEmprestimoByIdInstrumento(int idFigurino);
+
+        Task<IEnumerable<MovimentacaoFigurinoDTO>> GetAllByIdInstrumento(int idFigurino);
+
+        /// <summary>
+        /// Remove uma movimentação no banco de dados
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>
+        /// 200 - Sucesso <para />
+        /// 500 - Erro interno
+        /// </returns>
+        Task<int> DeleteAsync(int id);
     }
 }
