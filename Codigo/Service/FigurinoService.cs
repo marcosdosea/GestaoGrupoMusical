a06abc9093f397c75f0ed11df56735cbb2a9062e
reinan47 <reinanzinho100@gmail.com>
2023-08-08 03:29:44 -0300

    Criação da consulta com left Join . resolve #366

--- Codigo/Service/FigurinoService.cs
+++ Codigo/Service/FigurinoService.cs
@@ -91,5 +91,28 @@ namespace Service
 
             return figurino;
         }
+        public async Task<IEnumerable<Manequim>> GetAllManequim()
+        {
+            return await _context.Manequims.AsNoTracking().ToListAsync();
+        }
+        public async Task<IEnumerable<EstoqueDTO>> GetAllEstoqueDTO(int id)
+        {
+            var query = from figurino in _context.Figurinos
+                        where figurino.Id == id
+                        join figurinomanequim in _context.Figurinomanequims
+                        on figurino.Id equals figurinomanequim.IdFigurino into joinedFigurinoManequim //Criação do grupo com LEFT
+                        from figurinoManequim in joinedFigurinoManequim.DefaultIfEmpty()
+                        orderby figurinoManequim == null ? null : figurinoManequim.IdManequimNavigation.Tamanho
+                        select new EstoqueDTO
+                        {
+                            Nome = figurino.Nome,
+                            Data = figurino.Data,
+                            Tamanho = figurinoManequim == null ? null : figurinoManequim.IdManequimNavigation.Tamanho,
+                            Disponivel = figurinoManequim == null ? 0 : figurinoManequim.QuantidadeDisponivel,
+                            Entregues = figurinoManequim == null ? 0 : figurinoManequim.QuantidadeEntregue
+                        };
+
+            return await query.AsNoTracking().ToListAsync();
+        }
     }
 }
