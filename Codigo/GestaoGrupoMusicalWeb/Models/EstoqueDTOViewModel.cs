d6938232243e4ff25a34b42398095029c9b61849
reinan47 <reinanzinho100@gmail.com>
2023-08-08 03:28:14 -0300

    Criando o Model de EstoqueDTO

---
+++ Codigo/GestaoGrupoMusicalWeb/Models/EstoqueDTOViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestaoGrupoMusicalWeb.Models
+{
+    public class EstoqueDTOViewModel
+    {
+        [Display(Prompt = "Nome do figurino")]
+        public string? Nome { get; set; }
+        public DateTime? Data { get; set; }
+        [Display(Name = "Tamanho")]
+        public string? Tamanho { get; set; }
+        public int Disponivel { get; set; }
+        public int Entregues { get; set; }
+        public IEnumerable<EstoqueDTOViewModel>? TabelaEstoques { get; set; }
+    }
+}
