d7f79a40407fbf452a285893f6d63ba3e9e297f1
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2023-08-10 04:49:31 -0300

    Criação de uma DTO . #383

--- Codigo/Core/DTO/EnsaioDTO.cs
+++ Codigo/Core/DTO/EnsaioDTO.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Core.DTO
 {
@@ -12,5 +14,27 @@ namespace Core.DTO
         public string? Local { get; set; }
         public DateTime? DataHoraInicio { get; set; }
 
+    }
+
+    public class EnsaioDetailsDTO
+    {
+        public string? Tipo { get; set; }
+
+        [Display(Name = "Data e Hora de Início")]
+        public DateTime? DataHoraInicio { get; set; }
+
+        [Display(Name = "Data e Hora de Término")]
+        public DateTime? DataHoraFim { get; set; }
+
+        [Display(Name = "Presença Obrigatória")]
+        public string? PresencaObrigatoria { get; set; }
+        [Display(Prompt = "Informe o local do ensaio.")]
+        public string? Local { get; set; }
+
+        [Display(Name = "Repertório", Prompt = "Informe o repertório.")]
+        public string? Repertorio { get; set; }
+
+        [Display(Name = "Regente")]
+        public int NomeRegente { get; set; }
     }
 }
