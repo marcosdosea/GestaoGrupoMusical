c9855d7db7bb46c3ff50ae3ad1a95ab6105fa2c5
Lauro Santana Silva <devlauross@gmail.com>
2023-08-13 21:28:00 -0300

    Add propriedades

--- Codigo/Core/DTO/EnsaioFrequenciaDTO.cs
+++ Codigo/Core/DTO/EnsaioFrequenciaDTO.cs
@@ -1,7 +1,15 @@
-﻿
-namespace Core.DTO
+﻿namespace Core.DTO
 {
     public class EnsaioFrequenciaDTO
     {
+        public string Cpf { get; set; } = string.Empty;
+
+        public string NomeAssociado { get; set; } = string.Empty;
+
+        public string Justificativa { get; set; } = string.Empty;
+
+        public bool Presente { get; set; }
+
+        public bool JustificativaAceita { get; set; }
     }
 }
