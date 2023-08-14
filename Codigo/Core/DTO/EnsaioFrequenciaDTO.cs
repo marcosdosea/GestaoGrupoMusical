e00a85d86580ac17279e36915ce2932b528f8320
Lauro Santana Silva <devlauross@gmail.com>
2023-08-13 21:30:11 -0300

    Add display name

--- Codigo/Core/DTO/EnsaioFrequenciaDTO.cs
+++ Codigo/Core/DTO/EnsaioFrequenciaDTO.cs
@@ -1,15 +1,20 @@
-﻿namespace Core.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.DTO
 {
     public class EnsaioFrequenciaDTO
     {
+        [Display(Name = "CPF")]
         public string Cpf { get; set; } = string.Empty;
 
+        [Display(Name = "Associado")]
         public string NomeAssociado { get; set; } = string.Empty;
 
+        [Display (Name = "Justificativa Ausência")]
         public string Justificativa { get; set; } = string.Empty;
 
         public bool Presente { get; set; }
-
+        [Display(Name = "Justificativa Aceita")]
         public bool JustificativaAceita { get; set; }
     }
 }
