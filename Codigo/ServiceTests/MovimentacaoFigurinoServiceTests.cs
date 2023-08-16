f9119459a98d92896c7e029c3bd1b56cfa3697b9
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2023-08-16 11:31:01 -0300

    Criando dados para realizar os testes. #416

--- Codigo/ServiceTests/MovimentacaoFigurinoServiceTests.cs
+++ Codigo/ServiceTests/MovimentacaoFigurinoServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using Core;
 using Core.Service;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -15,5 +16,25 @@ namespace Service.Tests
         private GrupoMusicalContext _context;
         private IMovimentacaoFigurinoService movimentacaoFigurino;
 
+        [TestInitialize]
+        public void Initialize()
+        {
+            //Arrange
+            var builder = new DbContextOptionsBuilder<GrupoMusicalContext>();
+            builder.UseInMemoryDatabase("GrupoMusical");
+            var options = builder.Options;
+
+            _context = new GrupoMusicalContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+            var movimentacoesFigurinos = new List<Movimentacaofigurino>
+            {
+                new Movimentacaofigurino
+                {
+
+                }
+            };
+        }
     }
 }
