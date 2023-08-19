c4f3c174f66d9a5ff05cd068a726d72db64986e3
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2023-08-18 22:11:46 -0300

    Preenchimento do banco em memória com dados essencicais para realizar os testes. #430

--- Codigo/ServiceTests/FigurinoServiceTests.cs
+++ Codigo/ServiceTests/FigurinoServiceTests.cs
@@ -1,13 +1,64 @@
 ﻿using Core.Service;
 using Core;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
 
-namespace ServiceTests
+namespace Service.Tests
 {
     [TestClass]
     public class FigurinoServiceTests
     {
         private GrupoMusicalContext _context;
         private IFigurinoService _figurino;
+
+        [ClassInitialize]
+        public void Initialize()
+        {
+            // Arrange
+            var builder = new DbContextOptionsBuilder<GrupoMusicalContext>();
+            builder.UseInMemoryDatabase("GrupoMusical");
+            var options = builder.Options;
+
+            _context = new GrupoMusicalContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+            var figurinos = new List<Figurino>
+            {
+                new Figurino
+                {
+                    Id = 1,
+                    Nome = "Galinha Pintadinha",
+                    Data = new DateTime(2022, 8, 14, 0, 0, 0, 0, DateTimeKind.Local),
+                    IdGrupoMusical = 1
+                },
+                new Figurino
+                {
+                    Id = 2,
+                    Nome = "Mickey Mouse",
+                    Data = new DateTime(2022, 8, 14, 0, 0, 0, 0, DateTimeKind.Local),
+                    IdGrupoMusical = 1
+                },
+                new Figurino
+                {
+                    Id = 3,
+                    Nome = "Batman",
+                    Data = new DateTime(2022, 8, 14, 0, 0, 0, 0, DateTimeKind.Local),
+                    IdGrupoMusical = 1
+                },
+                new Figurino
+                {
+                    Id = 4,
+                    Nome = "Laterna Verde",
+                    Data = new DateTime(2022, 8, 14, 0, 0, 0, 0, DateTimeKind.Local),
+                    IdGrupoMusical = 1
+                },
+            };
+
+            _context.Figurinos.AddRange(figurinos);
+            _context.SaveChanges();
+
+            _figurino = new FigurinoService(_context);
+        }
     }
 }
