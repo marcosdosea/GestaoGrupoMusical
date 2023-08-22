d441588eef0bb775d3b281fd7ec3720be615201e
Lauro Santana Silva <devlauross@gmail.com>
2023-08-22 18:45:24 -0300

    Mudança no nome da view e add repertorio

--- Codigo/GestaoGrupoMusicalWeb/Controllers/EnsaioController.cs
+++ Codigo/GestaoGrupoMusicalWeb/Controllers/EnsaioController.cs
@@ -209,7 +209,7 @@ namespace GestaoGrupoMusicalWeb.Controllers
         }
 
         [Authorize(Roles = "ASSOCIADO")]
-        public async Task<ActionResult> RegistrarJustificativa ()
+        public async Task<ActionResult> EnsaiosAssociado ()
         {
             var model = await _ensaio.GetEnsaiosByIdPesoaAsync(Convert.ToInt32(User.FindFirst("Id")?.Value));
 
