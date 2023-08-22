5d10b6746e397e4b5b2679263da9056d91932bd9
Lauro Santana Silva <devlauross@gmail.com>
2023-08-22 20:44:25 -0300

    Add model para view de justificativa

---
+++ Codigo/GestaoGrupoMusicalWeb/Models/EnsaioJustificativaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoGrupoMusicalWeb.Models
+{
+    public class EnsaioJustificativaViewModel
+    {
+        public int IdEnsaio { get; set; }
+
+        [Display(Name = "Justificativa de Ausência", Prompt = "Informe a Justificativa...")]
+        public string Justificativa { get; set; } = string.Empty;
+    }
+}
