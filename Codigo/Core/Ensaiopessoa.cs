35b24037f04720fe793416ac87a4cb738bd5ee4d
matheus <matheusdacruzsouza1998@gmail.com>
2023-08-22 14:35:43 -0300

    atualizando

--- Codigo/Core/Ensaiopessoa.cs
+++ Codigo/Core/Ensaiopessoa.cs
@@ -10,8 +10,10 @@ namespace Core
         public sbyte Presente { get; set; }
         public string? JustificativaFalta { get; set; }
         public sbyte JustificativaAceita { get; set; }
+        public int IdPapelGrupoPapelGrupo { get; set; }
 
         public virtual Ensaio IdEnsaioNavigation { get; set; } = null!;
+        public virtual Papelgrupo IdPapelGrupoPapelGrupoNavigation { get; set; } = null!;
         public virtual Pessoa IdPessoaNavigation { get; set; } = null!;
     }
 }
--- Codigo/Core/Papelgrupo.cs
+++ Codigo/Core/Papelgrupo.cs
@@ -7,12 +7,16 @@ namespace Core
     {
         public Papelgrupo()
         {
+            Ensaiopessoas = new HashSet<Ensaiopessoa>();
+            Eventopessoas = new HashSet<Eventopessoa>();
             Pessoas = new HashSet<Pessoa>();
         }
 
         public int IdPapelGrupo { get; set; }
         public string Nome { get; set; } = null!;
 
+        public virtual ICollection<Ensaiopessoa> Ensaiopessoas { get; set; }
+        public virtual ICollection<Eventopessoa> Eventopessoas { get; set; }
         public virtual ICollection<Pessoa> Pessoas { get; set; }
     }
 }
