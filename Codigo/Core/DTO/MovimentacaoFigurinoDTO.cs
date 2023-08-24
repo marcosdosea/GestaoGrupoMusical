d196ba60af3b8e76d96c53505a76f156f21513d0
matheus <matheusdacruzsouza1998@gmail.com>
2023-08-24 16:10:17 -0300

    Adicionando campo quantidade e adcionando uma nova class movimentarConfirmacapoQuantidade

--- Codigo/Core/DTO/MovimentacaoFigurinoDTO.cs
+++ Codigo/Core/DTO/MovimentacaoFigurinoDTO.cs
@@ -28,7 +28,14 @@ namespace Core.DTO
 
         [Display(Name = "Movimentação")]
         public string Movimentacao { get; set; } = string.Empty;
-
+        [Display(Name ="Quantidade")]
+        public int QuantidadeEntregue { get; set; } 
         public string Status { get; set; } = string.Empty;
+    }
+
+    public class MovimentarConfirmaçãoQuantidade
+    {
+        public int Quantidade { get; set; }
+        public sbyte Confirmar { get; set; }
     }
 }
