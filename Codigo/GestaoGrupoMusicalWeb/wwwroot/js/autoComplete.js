2fbc462a16bfd3c5e64be308612da6f0e5a9687c
Lauro Santana Silva <devlauross@gmail.com>
2023-08-27 14:08:37 -0300

    Add uso de autocomplete em conjunto com select multiple

--- Codigo/GestaoGrupoMusicalWeb/wwwroot/js/autoComplete.js
+++ Codigo/GestaoGrupoMusicalWeb/wwwroot/js/autoComplete.js
@@ -1,4 +1,4 @@
-function fillAutocomplete(data, inputId) {
+function fillAutocomplete(data, inputId, listId) {
     if (data != null && inputId != null) {
         data = JSON.parse(data);
         const nomes = data.map((data) => {
@@ -6,7 +6,18 @@ function fillAutocomplete(data, inputId) {
         });
 
         $(`#${inputId}`).autocomplete({
-            source: nomes
+            source: nomes,
+            select: (event, ui) => {
+                var lista = document.getElementById(listId);
+
+                for (var i = 0; i < lista.options.length; i++) {
+                    if (lista.options[i].text == ui.item.value) {
+                        lista.options[i].selected = true;
+                        ui.item.value = "";
+                    }
+                }
+                
+            }
         });
     }
 }
