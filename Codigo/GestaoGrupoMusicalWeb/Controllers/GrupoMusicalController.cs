0f762a70729353f6aee18c33fc912bf0e6b0814b
Carlos Daniel <ms_danniel@outlook.com>
2023-09-01 15:34:21 -0300

    retornos status code nos switchs. #479

--- Codigo/GestaoGrupoMusicalWeb/Controllers/GrupoMusicalController.cs
+++ Codigo/GestaoGrupoMusicalWeb/Controllers/GrupoMusicalController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Net;
 
 namespace GestaoGrupoMusicalWeb.Controllers
 {
@@ -95,12 +96,12 @@ namespace GestaoGrupoMusicalWeb.Controllers
 
                 switch (await _grupoMusical.Create(grupoModel))
                 {
-                    case 200:
+                    case HttpStatusCode.OK:
 
                         Notificar("Grupo <b> Cadastrado </b> com <b> Sucesso </b> ", Notifica.Sucesso);
                         return RedirectToAction(nameof(Index));
                         break;
-                    case 500:
+                    case HttpStatusCode.InternalServerError:
                         Notificar("<b>Erro</b> ! Desculpe, ocorreu um erro durante o <b>Cadastro</b> do associado, se isso persistir entre em contato com o suporte", Notifica.Erro);
                         return RedirectToAction(nameof(Index));
                         break;
@@ -162,10 +163,10 @@ namespace GestaoGrupoMusicalWeb.Controllers
 
                 switch (await _grupoMusical.Edit(grupoMusical))
                 {
-                    case 200:
+                    case HttpStatusCode.OK:
                         Notificar("Grupo Musical <b>Editado</b> com <b>Sucesso</b>", Notifica.Sucesso);
                         break;
-                    case 500:
+                    case HttpStatusCode.InternalServerError:
                         Notificar("<b>Erro</b> ! Não foi possível editar as informações do grupo", Notifica.Erro);
                         break;
                 }
@@ -211,11 +212,11 @@ namespace GestaoGrupoMusicalWeb.Controllers
 
             switch (await _grupoMusical.Delete(id))
             {
-                case 200:
+                case HttpStatusCode.OK:
                     Notificar("Grupo <b> Excluido </b> com <b> Sucesso </b> ", Notifica.Sucesso);
                     return RedirectToAction(nameof(Index));
                     break;
-                case 500:
+                case HttpStatusCode.InternalServerError:
                     Notificar("<b>Erro</b> ! Desculpe, ocorreu um erro durante a <b>Exclusão</b> do Gruppo Musical, se isso persistir entre em contato com o suporte", Notifica.Erro);
                     return RedirectToAction(nameof(Index));
                     break;
