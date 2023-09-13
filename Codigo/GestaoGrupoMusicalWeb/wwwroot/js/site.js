9ee31d0c2981bce9dbe577ba5f366ceac3c8151a
Lauro Santana Silva <devlauross@gmail.com>
2023-09-13 17:38:38 -0300

    Add olho para mascarar ou remover mascara de senha

--- Codigo/GestaoGrupoMusicalWeb/wwwroot/js/site.js
+++ Codigo/GestaoGrupoMusicalWeb/wwwroot/js/site.js
@@ -5,30 +5,47 @@
 
 const toggleSenha = document.querySelector('#toggleSenha');
 const toggleConfirmSenha = document.querySelector('#toggleSenha2');
+const toggleSenhaAtual = document.querySelector('#toggleSenha3');
 const senhaInput = document.querySelector('#InputSenha');
 
-toggleSenha.addEventListener('click', function () {
-if (senhaInput.type === 'password') {
-    senhaInput.type = 'text';
-    toggleSenha.classList.remove('fa-eye');
-    toggleSenha.classList.add('fa-eye-slash');
-} else {
-    senhaInput.type = 'password';
-    toggleSenha.classList.remove('fa-eye-slash');
-    toggleSenha.classList.add('fa-eye');
-}
-});
-
-toggleConfirmSenha.addEventListener('click', function () {
-    if (InputConfirmarSenha.type === 'password') {
-        InputConfirmarSenha.type = 'text';
-        toggleSenha2.classList.remove('fa-eye');
-        toggleSenha2.classList.add('fa-eye-slash');
+if(toggleSenha != null){
+    toggleSenha.addEventListener('click', function () {
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+        toggleSenha.classList.remove('fa-eye');
+        toggleSenha.classList.add('fa-eye-slash');
     } else {
-        InputConfirmarSenha.type = 'password';
-        toggleSenha2.classList.remove('fa-eye-slash');
-        toggleSenha2.classList.add('fa-eye');
+        senhaInput.type = 'password';
+        toggleSenha.classList.remove('fa-eye-slash');
+        toggleSenha.classList.add('fa-eye');
     }
-});
+    });
+}
 
+if(toggleConfirmSenha != null){
+    toggleConfirmSenha.addEventListener('click', function () {
+        if (InputConfirmarSenha.type === 'password') {
+            InputConfirmarSenha.type = 'text';
+            toggleSenha2.classList.remove('fa-eye');
+            toggleSenha2.classList.add('fa-eye-slash');
+        } else {
+            InputConfirmarSenha.type = 'password';
+            toggleSenha2.classList.remove('fa-eye-slash');
+            toggleSenha2.classList.add('fa-eye');
+        }
+    });
+}
 
+if(toggleSenhaAtual != null){
+    toggleSenhaAtual.addEventListener('click', function () {
+        if (InputSenhaAtual.type === 'password') {
+            InputSenhaAtual.type = 'text';
+            toggleSenha3.classList.remove('fa-eye');
+            toggleSenha3.classList.add('fa-eye-slash');
+        } else {
+            InputSenhaAtual.type = 'password';
+            toggleSenha3.classList.remove('fa-eye-slash');
+            toggleSenha3.classList.add('fa-eye');
+        }
+    });
+}
