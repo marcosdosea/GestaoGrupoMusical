97f65250b09f5c958897f4351bca8802436eb354
Alesandro <105898363+Alesandr0@users.noreply.github.com>
2024-07-11 17:36:34 -0300

    Add files via upload

---
+++ Codigo/Core/DTO/InformativoDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Core.DTO
+{
+    public class InformativoIndexDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime? Data { get; set; }
+  
+        public string Mensagem { get; set; } = null!;
+
+    }
+}
+
+
+
