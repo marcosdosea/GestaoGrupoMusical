b229ced35751f8b32d22f13eb5940a7a80aac54e
Alesandro <105898363+Alesandr0@users.noreply.github.com>
2024-07-11 17:40:14 -0300

    Add files via upload

--- Codigo/Service/InformativoService.cs
+++ Codigo/Service/InformativoService.cs
@@ -1,5 +1,8 @@
 ﻿using Core;
+using Core.Datatables;
+using Core.DTO;
 using Core.Service;
+using Email;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
 
@@ -54,9 +57,9 @@ namespace Service
             }
         }
 
-        public Informativo? Get(uint id)
+        public async Task<Informativo?> Get(uint id)
         {
-            return _context.Informativos.Find(id);
+            return await _context.Informativos.FindAsync(id);
         }
 
         public async Task<IEnumerable<Informativo>> GetAll()
@@ -72,6 +75,83 @@ namespace Service
             return query;
         }
 
+        public DatatableResponse<InformativoIndexDTO> GetDataPage(DatatableRequest request, int idGrupo, IEnumerable<InformativoIndexDTO> InformativoIndexDTO)
+        {
+            var totalRecords = InformativoIndexDTO.Count();
+            if (request.Search != null && request.Search.GetValueOrDefault("value") != null)
+            {
+                InformativoIndexDTO = InformativoIndexDTO.Where(g => g.Mensagem.ToString().Contains(request.Search.GetValueOrDefault("value")!));
+            }
+
+            if (request.Order != null && request.Order[0].GetValueOrDefault("column")!.Equals("0"))
+            {
+                if (request.Order[0].GetValueOrDefault("dir")!.Equals("asc"))
+                {
+                    InformativoIndexDTO = InformativoIndexDTO.OrderByDescending(g => g.Data);
+                }
+                else
+                {
+                    InformativoIndexDTO = InformativoIndexDTO.OrderBy(g => g.Data);
+                }
+            }
+            else if (request.Order != null && request.Order[0].GetValueOrDefault("column")!.Equals("1"))
+            {
+                if (request.Order[0].GetValueOrDefault("dir")!.Equals("asc"))
+                    InformativoIndexDTO = InformativoIndexDTO.OrderBy(g => g.Mensagem);
+                else
+                    InformativoIndexDTO = InformativoIndexDTO.OrderByDescending(g => g.Mensagem);
+            }
+
+            int countRecordsFiltered = InformativoIndexDTO.Count();
+
+            InformativoIndexDTO = InformativoIndexDTO.Skip(request.Start).Take(request.Length);
+
+            return new DatatableResponse<InformativoIndexDTO>
+            {
+                Data = InformativoIndexDTO.ToList(),
+                Draw = request.Draw,
+                RecordsFiltered = countRecordsFiltered,
+                RecordsTotal = totalRecords
+            };
+        }
+
+
+        public async Task<HttpStatusCode> NotificarInformativoViaEmail(IEnumerable<PessoaEnviarEmailDTO> pessoas, uint idInformativo, string mensagem)
+        {
+            try
+            {
+                var informativo = await Get(idInformativo);
+                if (informativo != null) 
+                {
+                    List<EmailModel> emailsBody = new List<EmailModel>();
+                    foreach (PessoaEnviarEmailDTO p in pessoas)
+                    {
+                        emailsBody.Add(new EmailModel()
+                        {
+                            Assunto = $"Batalá - Notificação de Novo Informativo",
+                            AddresseeName = p.Nome,
+                            Body = "<div style=\"text-align: center;\">\r\n    " +
+                                $"<h3>" + mensagem + "</h3>\r\n</div>",
+                            To = new List<string> { p.Email }
+                        });
+                    }
+
+                    List<Task> emailTask = new List<Task>();
+                    foreach(EmailModel email in emailsBody)
+                    {
+                        emailTask.Add(EmailService.Enviar(email));
+                    }
+
+                    return HttpStatusCode.OK;
+                }
+
+                return HttpStatusCode.NotFound;
+            } 
+            catch
+            {
+                return HttpStatusCode.InternalServerError;
+            }
+        }
 
     }
 }
