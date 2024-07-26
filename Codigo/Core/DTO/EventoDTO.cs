a0078776448463f3c89c96fdb023b00ae37a9c4b
gilmario <gilmariosantos1@gmail.com>
2024-07-25 21:58:55 -0300

    Atualização dos campos de listar instrumentos e quantidade, na view.

--- Codigo/Core/DTO/EventoDTO.cs
+++ Codigo/Core/DTO/EventoDTO.cs
@@ -22,29 +22,4 @@ namespace Core.DTO
         [Display(Name = "Confirmados")]
         public int Confirmados { get; set; }
     }
-
-
-    public class GerenciarInstrumentoEventoDTO
-    {
-        public int Id { get; set; }
-
-        [Display(Name = "Data hora início")]
-        public DateTime DataHoraInicio { get; set; }
-
-        [Display(Name = "Data hora fim")]
-        public DateTime DataHoraFim { get; set; }
-
-        [Display(Name = "Regente")]
-        public IEnumerable<string>? Regente { get; set; }
-
-        [Display(Name = "Figurino")]
-        public IEnumerable<string>? Figurino { get; set; }
-
-        [Display(Name = "Local")]
-        public string? Local { get; set; }
-
-        [Display(Name = "Instrumento")]
-        public IEnumerable<Tipoinstrumento>? Instrumento { get; set; }
-
-    }
 }
