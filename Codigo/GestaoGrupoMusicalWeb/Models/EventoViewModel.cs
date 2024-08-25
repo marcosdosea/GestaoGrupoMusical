2ce86dd52625759c89002e8bfed826847892bb74
gilmario <gilmariosantos1@gmail.com>
2024-08-25 08:44:24 -0300

    Implementando form para adicionar instrumento planejado em eventos.

--- Codigo/GestaoGrupoMusicalWeb/Models/EventoViewModel.cs
+++ Codigo/GestaoGrupoMusicalWeb/Models/EventoViewModel.cs
@@ -112,10 +112,22 @@ namespace GestaoGrupoMusicalWeb.Models
         public SelectList? ListaInstrumentos { get; set; }
         public int Quantidade { get; set; }
         public IEnumerable<GerenciarInstrumentoEventoDTO>? GerenciarInstrumentos { get; set; }        
+            
 
     }
+    public class PlanejarInstrumentoEventoViewModel
+    {
+        public int Id { get; set; }
 
-    public class GerenciarSolicitacaoEventoViewModel
+        public int IdInstrumento { get; set; }
+
+        [Display(Name = "Instrumento")]
+        public SelectList? ListaInstrumentos { get; set; }
+        public int Quantidade { get; set; }
+        public IEnumerable<GerenciarInstrumentoEventoDTO>? InstrumentoEventoPlanejado { get; set; }
+    }
+
+        public class GerenciarSolicitacaoEventoViewModel
     {
         //esse ID é do evento
         public int Id { get; set; }
