7788d5d98bdab6c04afd4b9818fe0ef1db64d7e4
Alesandro <105898363+Alesandr0@users.noreply.github.com>
2024-08-31 11:30:48 -0300

    Update EventoViewModel.cs

--- Codigo/GestaoGrupoMusicalWeb/Models/EventoViewModel.cs
+++ Codigo/GestaoGrupoMusicalWeb/Models/EventoViewModel.cs
@@ -1,4 +1,4 @@
-﻿using Core;
+using Core;
 using Core.DTO;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.ComponentModel.DataAnnotations;
@@ -9,34 +9,48 @@ namespace GestaoGrupoMusicalWeb.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Display(Name = "Grupo Musical")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public int IdGrupoMusical { get; set; }
+
         [Display(Name = "Data hora início")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public DateTime DataHoraInicio { get; set; }
+
         [Display(Name = "Data Hora Fim")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public DateTime DataHoraFim { get; set; }
+
         [Display(Name = "Local")]
         public string? Local { get; set; }
+
         [Display(Name = "Repetório")]
         public string? Repertorio { get; set; }
+
         [Display(Name = "Colaborador Reponsável")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public int IdColaboradorResponsavel { get; set; }
+
         [Display(Name = "Regente")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public int IdRegente { get; set; }
 
+        [Display(Name = "Figurinos")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public int IdFigurinoSelecionado { get; set; }
+
         [Display(Name = "Regentes")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public IEnumerable<int>? IdRegentes { get; set; }
+
         public string? JsonLista { get; set; }
         public IEnumerable<FigurinoDropdownDTO> FigurinoList { get; set; } = null!;
 
         public SelectList? ListaPessoa { get; set; }
-        public SelectList? ListaGrupoMusical { get; set; }       
+        public SelectList? ListaGrupoMusical { get; set; }
+        public SelectList? ListaFigurino { get; set; }
+        public List<AssociadoDTO> AssociadosDTO { get; set; }
 
     }
 
@@ -73,6 +87,8 @@ namespace GestaoGrupoMusicalWeb.Models
         public string? JsonLista { get; set; }
 
         public SelectList? ListaPessoa { get; set; }
+        public string Justificativa { get; set; }
+
 
     }
     public class GerenciarInstrumentoEventoViewModel
@@ -99,19 +115,19 @@ namespace GestaoGrupoMusicalWeb.Models
 
         [Display(Name = "Local")]
         public string? Local { get; set; }
-        
+
         [Display(Name = "Regentes")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        public IEnumerable<int>? IdRegentes { get; set; }              
+        public IEnumerable<int>? IdRegentes { get; set; }
 
         public string? JsonLista { get; set; }
 
         public SelectList? ListaPessoa { get; set; }
-        
+
         [Display(Name = "Instrumento")]
         public SelectList? ListaInstrumentos { get; set; }
         public int Quantidade { get; set; }
-        public IEnumerable<GerenciarInstrumentoEventoDTO>? GerenciarInstrumentos { get; set; }       
+        public IEnumerable<GerenciarInstrumentoEventoDTO>? GerenciarInstrumentos { get; set; }
 
     }
     public class PlanejarInstrumentoEventoViewModel
@@ -126,7 +142,7 @@ namespace GestaoGrupoMusicalWeb.Models
         public IEnumerable<InstrumentoPlanejadoEventoDTO>? InstrumentoEventoPlanejado { get; set; }
     }
 
-        public class GerenciarSolicitacaoEventoViewModel
+    public class GerenciarSolicitacaoEventoViewModel
     {
         //esse ID é do evento
         public int Id { get; set; }
@@ -140,10 +156,35 @@ namespace GestaoGrupoMusicalWeb.Models
         public DateTime DataHoraFim { get; set; }
 
         [Display(Name = "Regentes")]
-        public string NomesRegentes { get;set; } = null!;
-
-        public int FaltasPessoasEmEnsaioMeses { get; set; }
+        public string NomesRegentes { get; set; } = null!;
         public IEnumerable<SolicitacaoEventoPessoasDTO>? EventoSolicitacaoPessoasDTO { get; set; }
+    }
+    public class FrequenciaEventoViewModel
+    {
+        public int Id { get; set; }
+        public int IdGrupoMusical { get; set; }
+
+        [Display(Name = "Início")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public DateTime? DataHoraInicio { get; set; }
+
+        [Display(Name = "Final")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public DateTime? DataHoraFim { get; set; }
+
+        [Display(Name = "Regentes")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public IEnumerable<int>? IdRegentes { get; set; }
+
+        public SelectList? ListaPessoa { get; set; }
+
+        public SelectList? ListaFigurino { get; set; }
+
+        [Display(Name = "Local")]
+        [MaxLength(100, ErrorMessage = "O campo {0} deve ter no máximo 100 caracteres")]
+        public string? Local { get; set; }
+
+        public string? JsonLista { get; set; }
     }
 
 }
