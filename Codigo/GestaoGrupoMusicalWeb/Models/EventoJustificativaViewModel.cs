80ead2d90cab16e18f83ed414f0a11397e72a63b
Alesandro <105898363+Alesandr0@users.noreply.github.com>
2024-08-31 11:32:45 -0300

    Criada a pasta EventoJustificativaViewModel

---
+++ Codigo/GestaoGrupoMusicalWeb/Models/EventoJustificativaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoGrupoMusicalWeb.Models
+{
+    public class EventoJustificativaViewModel
+    {
+        public int IdEvento { get; set; }
+
+        [Display(Name = "Justificativa de Ausência", Prompt = "Informe a Justificativa...")]
+        [MaxLength(200, ErrorMessage = "O Campo {0} deve ter no máximo 100 caracteres")]
+        public string? Justificativa { get; set; }
+    }
+}
