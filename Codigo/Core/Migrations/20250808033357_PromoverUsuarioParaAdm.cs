b530b98adf64503ac9e65ce8749d9e998d89707b
Paulo Vitor <paulovitormengao2015@gmail.com>
2025-08-08 00:59:31 -0300

    Admnistrador do Sistema criado para testes.

---
+++ Codigo/Core/Migrations/20250808033357_PromoverUsuarioParaAdm.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class PromoverUsuarioParaAdm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE aspnetuserroles SET RoleId = (SELECT Id FROM aspnetroles WHERE Name = 'ADMINISTRADOR SISTEMA') WHERE UserId = (SELECT Id FROM aspnetusers WHERE Email = 'caioteste949@gmail.com');");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE aspnetuserroles SET RoleId = (SELECT Id FROM aspnetroles WHERE Name = 'REGENTE') WHERE UserId = (SELECT Id FROM aspnetusers WHERE Email = 'caioteste949@gmail.com');");
+        }
+    }
+}
---
+++ Codigo/Core/Migrations/20250808035321_AtualizarPapelGrupoNaPessoa.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizarPapelGrupoNaPessoa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE pessoa SET idPapelGrupo = (SELECT idPapelGrupo FROM papelgrupo WHERE nome = 'ADMINISTRADOR SISTEMA') WHERE email = 'caioteste949@gmail.com';");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE pessoa SET idPapelGrupo = (SELECT idPapelGrupo FROM papelgrupo WHERE nome = 'REGENTE') WHERE email = 'caioteste949@gmail.com';");
+        }
+    }
+}
