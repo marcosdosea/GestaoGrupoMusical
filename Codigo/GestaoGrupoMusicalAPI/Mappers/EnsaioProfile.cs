45ccbd644b5fe8afdacc27eadb5ae39eb7bd556b
Talysson Vasconcelos <talyssonpestinha.tp@gmail.com>
2026-03-03 12:28:08 -0300

    fix(mapper): configurar perfis do AutoMapper para evitar MappingException

--- Codigo/GestaoGrupoMusicalAPI/Mappers/EnsaioProfile.cs
+++ Codigo/GestaoGrupoMusicalAPI/Mappers/EnsaioProfile.cs
@@ -1,17 +1,19 @@
 ﻿using AutoMapper;
 using Core;
 using Core.DTO;
-using GestaoGrupoMusicalAPI.Models;
 
-namespace GestaoGrupoMusicalAPI.Mapper
+public class EnsaioProfile : Profile
 {
-    public class EnsaioProfile : Profile
+    public EnsaioProfile()
     {
-        public EnsaioProfile()
-        {   
-            CreateMap<Ensaio, EnsaioDTO>().ReverseMap();
-            CreateMap<Ensaio, EnsaioDetailsDTO>().ReverseMap();
-        }
+        CreateMap<EnsaioDetailsDTO, EnsaioAssociadoDTO>()
+            .ForMember(dest => dest.Inicio, opt => opt.MapFrom(src => src.DataHoraInicio))
+            .ForMember(dest => dest.Fim, opt => opt.MapFrom(src => src.DataHoraFim))
+            .ForMember(dest => dest.Presente, opt => opt.MapFrom(_ => false))
+            .ForMember(dest => dest.JustificativaAceita, opt => opt.MapFrom(_ => false));
+
+        CreateMap<EnsaioAssociadoDTO, EventosEnsaiosAssociadoDTO>()
+            .ForMember(dest => dest.Ensaios, opt => opt.MapFrom(src => new List<EnsaioAssociadoDTO> { src }))
+            .ForMember(dest => dest.Eventos, opt => opt.MapFrom(_ => new List<EventoAssociadoDTO>()));
     }
 }
-
