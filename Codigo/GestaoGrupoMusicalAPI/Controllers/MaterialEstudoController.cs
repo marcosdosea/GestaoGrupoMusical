8313167d71da9f459782b742f50f171a4d730e1c
TalyssonDEV <talyssonpestinha.TP@gmail.com>
2026-03-06 22:28:10 -0300

    feat: adicionar controller de Material de Estudo com filtro por Claims

---
+++ Codigo/GestaoGrupoMusicalAPI/Controllers/MaterialEstudoController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Core.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoGrupoMusicalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "REGENTE")]
+    public class MaterialEstudoController : Controller
+    {
+
+        private readonly IMaterialEstudoService material;
+        private readonly IMapper mapper;
+
+        public MaterialEstudoController(IMaterialEstudoService material, IMapper mapper)
+        {
+            this.material = material;
+            this.mapper = mapper;
+        }
+
+        // GET: MaterialEstudoController
+        [HttpGet]
+        public async Task<ActionResult> GetAsync()
+        {
+
+            var idGrupoClaim = User.Claims.FirstOrDefault(c => c.Type == "IdGrupoMusical")?.Value;
+
+            if (idGrupoClaim == null) return Unauthorized("Grupo não indentificado");
+
+            int idGrupo = int.Parse(idGrupoClaim);
+
+            var listaMaterialEstudo = await material.GetAllMaterialEstudoPerIdGrupo(idGrupo);
+
+            if(listaMaterialEstudo == null) return NotFound("Lista de Materias de Estudo vázia");
+  
+            return Ok(listaMaterialEstudo);
+        }
+
+    }
+}
