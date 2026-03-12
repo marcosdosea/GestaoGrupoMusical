1fb4777e9a6d9dc31d2ade243c9d795705287d30
TalyssonDEV <talyssonpestinha.TP@gmail.com>
2026-03-11 21:23:31 -0300

    Arquivos base

---
+++ Codigo/GestaoGrupoMusicalMobile/lib/config/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFD64550); // Rosa Batalá
+  static const Color secondary = Color(0xFFD64550); // AppBar
+  static const Color background = Color(0xFFF8F8F8);
+  static const Color cardBackground = Colors.white;
+  static const Color textLight = Colors.white70;
+}
