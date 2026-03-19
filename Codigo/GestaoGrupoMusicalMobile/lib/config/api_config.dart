a52ed2dae03a1dc676b6852ea7ed0351c466f5aa
TalyssonDEV <talyssonpestinha.TP@gmail.com>
2026-03-19 16:03:27 -0300

    feat: adaptar ApiConfig para identificar ambiente automaticamente

--- Codigo/GestaoGrupoMusicalMobile/lib/config/api_config.dart
+++ Codigo/GestaoGrupoMusicalMobile/lib/config/api_config.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
 class ApiConfig {
-  static const String baseUrl = "https://localhost:7125"; 
+  static String get baseUrl{
+    if (Platform.isAndroid) {
+      return "https://192.168.0.103:7125"; 
+    }
+    return "https://localhost:7125";
+  }
   static const Duration timeout = Duration(seconds: 30);
 }
