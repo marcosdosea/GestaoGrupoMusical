33a7602cf2ab74ebcb787bc4ef7797a2e7c1ed85
Talysson Vasconcelos <talyssonpestinha.tp@gmail.com>
2026-03-26 21:47:20 -0300

    chore: atualiza configuracoes de api, dependencias e ajusta layout da home. Close #871

--- Codigo/GestaoGrupoMusicalMobile/lib/config/api_config.dart
+++ Codigo/GestaoGrupoMusicalMobile/lib/config/api_config.dart
@@ -3,8 +3,9 @@ import 'dart:io';
 class ApiConfig {
   static String get baseUrl{
     if (Platform.isAndroid) {
-      return "https://192.168.0.103:7125"; 
+      return "http://192.168.0.108:5153"; 
     }
+    if((Platform.isWindows)){}
     return "https://localhost:7125";
   }
   static const Duration timeout = Duration(seconds: 30);
