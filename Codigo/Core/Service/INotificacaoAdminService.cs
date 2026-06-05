2f62a2f4887eb18dc7d71eca9d3fef74baeef979
Talysson Vasconcelos <talyssonpestinha.tp@gmail.com>
2026-06-05 14:05:57 -0300

    feat(api): infraestrutura básica de notificações (Interfaces)

---
+++ Codigo/Core/Service/INotificacaoAdminService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface INotificacaoAdminService
+    {
+        Task Enviar(string token, string titulo, string corpo);
+    }
+}
