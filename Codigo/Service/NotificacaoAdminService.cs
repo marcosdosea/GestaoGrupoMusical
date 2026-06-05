d5091da7379309eaf8e029dfdf92fb48e6ae71f6
Talysson Vasconcelos <talyssonpestinha.tp@gmail.com>
2026-06-05 14:10:45 -0300

    feat(api): implementação do DispositivoService e NotificacaoAdminService

---
+++ Codigo/Service/NotificacaoAdminService.cs
@@ -0,0 +1,23 @@
+﻿using Core.Service;
+using FirebaseAdmin.Messaging;
+
+namespace Service
+{
+    public class NotificacaoAdminService : INotificacaoAdminService
+    {
+        public async Task Enviar(string token, string titulo, string corpo)
+        {
+            var message = new Message()
+            {
+                Token = token,
+                Notification = new Notification()
+                {
+                    Title = titulo,
+                    Body = corpo,
+                },
+            };
+
+            await FirebaseMessaging.DefaultInstance.SendAsync(message);
+        }
+    }
+}
