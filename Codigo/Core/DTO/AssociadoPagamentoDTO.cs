f28ba179a6d254fae3a733983116258a1564a627
Iago <liberato_resende@hotmail.com>
2026-07-23 13:09:10 -0300

    Correção do criar evento e da parte financeira

--- Codigo/Core/DTO/AssociadoPagamentoDTO.cs
+++ Codigo/Core/DTO/AssociadoPagamentoDTO.cs
@@ -11,7 +11,7 @@ namespace Core.DTO
         public int IdAssociado { get; set; }
         public string NomeAssociado { get; set; } = string.Empty;
         public string? Cpf { get; set; }
-        public DateTime DataPagamento { get; set; }
+        public DateTime? DataPagamento { get; set; }
         public decimal ValorPago { get; set; }
         public string? Observacoes { get; set; }
         public string Status { get; set; } = "NAO_PAGOU"; // Valor Padrão
